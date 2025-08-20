f2e53575672b5abecc0b22bc35b91dc31afc891c
daiji <jialu.dai@edu.unifi.it>
2025-08-20 22:32:37 +0200

    add getter&setter

--- src/main/java/com/ledger/ledger/domain/CreditAccount.java
+++ src/main/java/com/ledger/ledger/domain/CreditAccount.java
@@ -11,23 +11,22 @@ import java.util.List;
 @Table(name = "credit_account")
 public class CreditAccount extends Account {
 
-    @Column(name = "credit_limit", precision = 15, scale = 2, nullable = true)
+    @Column(name = "credit_limit", precision = 15, scale = 2, nullable = false)
     private BigDecimal creditLimit= BigDecimal.ZERO;
 
     @Column(name = "current_debt", precision = 15, scale = 2, nullable = true)
     private BigDecimal currentDebt = BigDecimal.ZERO;
 
     @Column(name = "bill_date")
-    private MonthDay billDate;
+    private Integer billDay=null;
 
     @Column(name = "due_date")
-    private MonthDay dueDate;
+    private Integer dueDay=null;
 
     @OneToMany(mappedBy = "linkedAccount", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<InstallmentPlan> installmentPlans;
 
-    public CreditAccount(){
-    }
+    public CreditAccount(){}
     public CreditAccount(String name,
                          BigDecimal balance,
                          User owner,
@@ -36,13 +35,17 @@ public class CreditAccount extends Account {
                          boolean selectable,
                          BigDecimal creditLimit,
                          BigDecimal currentDebt, //contiene importo residuo delle rate
-                         MonthDay billDate,
-                         MonthDay dueDate, AccountType type) {
+                         Integer billDate,
+                         Integer dueDate, AccountType type) {
         super(name, balance, type, AccountCategory.CREDIT, owner, notes, includedInNetWorth, selectable);
         this.creditLimit = creditLimit;
-        this.currentDebt = currentDebt;
-        this.billDate = billDate;
-        this.dueDate = dueDate;
+        if(currentDebt == null){
+            this.currentDebt=BigDecimal.ZERO;
+        }else {
+            this.currentDebt = currentDebt;
+        }
+        this.billDay = billDate;
+        this.dueDay = dueDate;
         this.installmentPlans = new ArrayList <>();
         this.owner.addAccount(this);
     }
@@ -50,27 +53,38 @@ public class CreditAccount extends Account {
     public BigDecimal getCurrentDebt() {
         return currentDebt;
     }
+    public BigDecimal getCreditLimit(){return creditLimit;}
+    public Integer getBillDay(){return billDay;}
+    public Integer getDueDay(){return dueDay;}
+    public void setCurrentDebt(BigDecimal currentDebt) {
+        this.currentDebt = currentDebt;
+    }
+    public void setCreditLimit(BigDecimal creditLimit) {
+        this.creditLimit = creditLimit;
+    }
+    public void setBillDay(Integer billDate) {
+        this.billDay = billDate;
+    }
+    public void setDueDay(Integer dueDate) {
+        this.dueDay = dueDate;
+    }
 
     @Override
     public void debit(BigDecimal amount) {
         if (amount.compareTo(balance) > 0) { //amount>balance
             if (currentDebt.add(amount.subtract(balance)).compareTo(creditLimit) > 0) { //currentDebt+(amount-balance)>creditLimit
                 throw new IllegalArgumentException("Amount exceeds credit limit");
-            }else{
+            } else {
                 balance = BigDecimal.ZERO;
                 //addNewDebt(amount.subtract(balance));
                 currentDebt = currentDebt.add(amount.subtract(balance));
             }
-        }else{
+        } else {
             balance = balance.subtract(amount);
         }
         this.owner.updateTotalLiabilities();
         this.owner.updateTotalAssets();
         this.owner.updateNetAsset();
-    }
-    public void addNewDebt(BigDecimal amount) {
-        currentDebt = currentDebt.add(amount);
-        //this.owner.updateNetAssetsAndLiabilities(amount);
     }
 
     public void repayDebt(BigDecimal amount, Account fromAccount) {
