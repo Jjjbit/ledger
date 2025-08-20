8a49e0942a985ff3c7e5a932d48c929befb4dc13
daiji <jialu.dai@edu.unifi.it>
2025-08-20 22:36:59 +0200

    add UserRepository

--- src/main/java/com/ledger/ledger/repository/UserRepository.java
+++ src/main/java/com/ledger/ledger/repository/UserRepository.java
@@ -2,12 +2,9 @@ package com.ledger.ledger.repository;
 
 import com.ledger.ledger.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-
     User findByUsername(String name);
 }
