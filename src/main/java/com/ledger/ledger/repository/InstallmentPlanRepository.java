8143839b566bfbaab988d3fa14069d20b2cbffc3
daiji <jialu.dai@edu.unifi.it>
2025-08-21 00:24:54 +0200

    add InstallmentPlanRepository

--- src/main/java/com/ledger/ledger/repository/InstallmentPlanRepository.java
+++ src/main/java/com/ledger/ledger/repository/InstallmentPlanRepository.java
@@ -2,7 +2,9 @@ package com.ledger.ledger.repository;
 
 import com.ledger.ledger.domain.InstallmentPlan;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface InstallmentPlanRepository extends JpaRepository<InstallmentPlan, Long> {
 
 }
