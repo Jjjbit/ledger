109fc8e12cd68614135d04b24a433846cea94b63
daiji <jialu.dai@edu.unifi.it>
2025-08-20 22:36:38 +0200

    add LedgerRepository

--- src/main/java/com/ledger/ledger/repository/LedgerRepository.java
+++ src/main/java/com/ledger/ledger/repository/LedgerRepository.java
@@ -2,6 +2,9 @@ package com.ledger.ledger.repository;
 
 import com.ledger.ledger.domain.Ledger;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface LedgerRepository extends JpaRepository<Ledger, Long> {
+
 }
