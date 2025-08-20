e300d72fdb945c08d16b39b29ecd8d09844eceff
daiji <jialu.dai@edu.unifi.it>
2025-08-21 00:24:38 +0200

    add findByParentIsNull(), findByName()

--- src/main/java/com/ledger/ledger/repository/CategoryComponentRepository.java
+++ src/main/java/com/ledger/ledger/repository/CategoryComponentRepository.java
@@ -1,10 +1,15 @@
 package com.ledger.ledger.repository;
 
+import com.ledger.ledger.domain.Category;
 import com.ledger.ledger.domain.CategoryComponent;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.StringJoiner;
+
 @Repository
 public interface CategoryComponentRepository extends JpaRepository<CategoryComponent, Long> {
-
+    List<Category> findByParentIsNull();
+    CategoryComponent findByName(String name);
 }
