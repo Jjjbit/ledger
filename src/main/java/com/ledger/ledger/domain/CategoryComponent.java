09d217ddffdd87f39ca3ab7862da5a66e26a2c54
daiji <jialu.dai@edu.unifi.it>
2025-08-23 00:52:55 +0200

    modify of type

--- src/main/java/com/ledger/ledger/domain/CategoryComponent.java
+++ src/main/java/com/ledger/ledger/domain/CategoryComponent.java
@@ -11,9 +11,6 @@ import java.util.List;
 @Inheritance(strategy= InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "level", discriminatorType = DiscriminatorType.STRING)
 public abstract class CategoryComponent {
-    public enum CategoryType {
-        INCOME, EXPENSE, ROOT
-    }
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -23,13 +20,7 @@ public abstract class CategoryComponent {
     protected String name;
 
     @Column(length = 20, nullable = false)
-    protected CategoryType type; //"income", "expense", "root"
-
-    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL, orphanRemoval = true)
-    protected List<Transaction> transactions = new ArrayList<>(); //un category -> più transazioni.
-
-    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Budget> budgets = new ArrayList<>();
+    protected CategoryType type; //"income", "expense"
 
     @ManyToOne
     @JoinColumn(name = "parent_id")
@@ -44,54 +35,22 @@ public abstract class CategoryComponent {
     public abstract void add(CategoryComponent child);
     public abstract List<CategoryComponent> getChildren();
     public abstract void display(String indent);
-    public CategoryComponent getParent(){
-        return this.parent;
+
+    public CategoryComponent getParent() {
+        return parent;
     }
+
     public void setParent(CategoryComponent parent) {
         this.parent = parent;
     }
     public String getName() {
         return name;
     }
+    public void setName(String name){this.name=name;}
     public CategoryType getType() {
         return type;
     }
-    //public abstract void changeLevel(CategoryComponent root);
-
-    public void addTransaction(Transaction t) {
-        transactions.add(t);
+    public Long getId() {
+        return id;
     }
-    public void addBudget(Budget b) {
-        budgets.add(b);
-    }
-
-    //returns list of transactions of the category and its subcategories in period
-    public List<Transaction> getTransactionsInPeriod(Budget.Period period, LocalDate startDate) {
-        return transactions.stream()
-                .filter(t -> switch (period) {
-                    case MONTHLY -> t.getDate().isBefore(startDate.plusMonths(1));
-                    case WEEKLY -> t.getDate().isBefore(startDate.plusWeeks(1));
-                    case YEARLY -> t.getDate().isBefore(startDate.plusYears(1));
-                })
-                .toList();
-    }
-
-    //returns list of budget for the category and its subcategories in period
-    public List<Budget> getBudgetsForPeriod(Budget.Period p) {
-        return budgets.stream()
-                .filter(b -> b.getPeriod() == p)
-                .toList();
-    }
-
-    //returns total budget for the category and its subcategories in period
-    public BigDecimal getTotalBudgetForPeriod(Budget.Period period) {
-        return budgets.stream()
-                .filter(b -> b.getPeriod() == period)
-                .map(Budget::getAmount)
-                .reduce(BigDecimal.ZERO, BigDecimal::add);
-    }
-
-    public abstract List<Transaction> getTransactions();
-
-    public abstract void printTransactionSummary();
 }
