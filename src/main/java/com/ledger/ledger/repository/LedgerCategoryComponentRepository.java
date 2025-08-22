6e216989e876c51984cafd28ee6041c9d3ae9168
daiji <jialu.dai@edu.unifi.it>
2025-08-23 00:51:55 +0200

    add existsByLedgerAndName(), findByLedgerAndName()

--- src/main/java/com/ledger/ledger/repository/LedgerCategoryComponentRepository.java
+++ src/main/java/com/ledger/ledger/repository/LedgerCategoryComponentRepository.java
@@ -1,9 +1,13 @@
 package com.ledger.ledger.repository;
 
+import com.ledger.ledger.domain.Ledger;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.ledger.ledger.domain.LedgerCategoryComponent;
 
 @Repository
 public interface LedgerCategoryComponentRepository extends JpaRepository<LedgerCategoryComponent, Long> {
+    LedgerCategoryComponent findByName(String name);
+    boolean existsByLedgerAndName(Ledger ledger, String name);
+    LedgerCategoryComponent findByLedgerAndName(Ledger ledger,  String name);
 }
