5b53668c9f2c4b12b0291b2c9ae9f2062cb650cd
daiji <jialu.dai@edu.unifi.it>
2025-08-23 00:49:07 +0200

    modify cascade

--- src/main/java/com/ledger/ledger/domain/Ledger.java
+++ src/main/java/com/ledger/ledger/domain/Ledger.java
@@ -3,7 +3,9 @@ package com.ledger.ledger.domain;
 import jakarta.persistence.*;
 
 import java.math.BigDecimal;
+import java.time.YearMonth;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 @Entity
@@ -19,13 +21,13 @@ public class Ledger {
     @JoinColumn(name = "owner_id")
     private User owner;
 
-    @OneToMany(mappedBy = "ledger", cascade = CascadeType.ALL, orphanRemoval = true)
+    @OneToMany(mappedBy = "ledger", cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE}, orphanRemoval = true)
     private List<Transaction> transactions=new ArrayList<>(); //relazione tra Transaction e Ledger è composizione
 
-    @OneToMany(mappedBy = "ledger", cascade = CascadeType.ALL, orphanRemoval = true)
+    @OneToMany(mappedBy = "ledger", cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE}, orphanRemoval = true)
     private List<LedgerCategoryComponent> categories = new ArrayList<>();
 
-    @OneToMany(mappedBy = "ledger", cascade = CascadeType.ALL, orphanRemoval = false)
+    @OneToMany(mappedBy = "ledger", cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE}, orphanRemoval = true)
     private List<BorrowingAndLending> borrowingAndLendings = new ArrayList<>(); // relazione tra LoanRecord e Ledger è aggregazione
 
     @Column(name = "total_income", precision = 15, scale = 2, nullable = false)
@@ -44,12 +46,11 @@ public class Ledger {
     public User getOwner(){return this.owner;}
     public void addTransaction(Transaction tx) {
         transactions.add(tx); // Aggiunge una transazione al ledger
-        tx.execute();
     }
     public List<Transaction> getTransactions() {
         return transactions;
     }
-    public List<LedgerCategoryComponent> getCategories(){return categories;}
+    public List<LedgerCategoryComponent> getCategoryComponents(){return categories;}
 
     public void removeTransaction(Transaction tx) {
         transactions.remove(tx); // Rimuove una transazione dal ledger
@@ -62,9 +63,62 @@ public class Ledger {
     }
 
 
-    public void addCategory(LedgerCategoryComponent category) {
+    public void addCategoryComponent(LedgerCategoryComponent category) {
         categories.add(category);
         category.setLedger(this);
+    }
+
+    //does not matter income or expense
+    public List<Transaction> getTransactionsForMonth(YearMonth month) {
+        return transactions.stream()
+                .filter(t -> t.getDate().getYear() == month.getYear() && t.getDate().getMonth() == month.getMonth())
+                .sorted(Comparator.comparing(Transaction::getDate).reversed())
+                .toList();
+    }
+    public List<Transaction> getTransactionsForYear(int year) {
+        return transactions.stream()
+                .filter(tx -> tx.getDate().getYear() == year)
+                .sorted(Comparator.comparing(Transaction::getDate).reversed())
+                .toList();
+    }
+
+    public BigDecimal getTotalIncomeForMonth(YearMonth month) {
+        return getTransactionsForMonth(month).stream()
+                .filter(tx -> tx.getType() == TransactionType.INCOME)
+                .map(Transaction::getAmount)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+
+    }
+    public BigDecimal getTotalExpenseForMonth(YearMonth month) {
+        return getTransactionsForMonth(month).stream()
+                .filter(tx ->tx.getType() == TransactionType.EXPENSE)
+                .map(Transaction::getAmount)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+    }
+
+    public BigDecimal getRemainingForMonth(YearMonth month) {
+        BigDecimal totalIncome = getTotalIncomeForMonth(month);
+        BigDecimal totalExpense = getTotalExpenseForMonth(month);
+        return totalIncome.subtract(totalExpense);
+
+    }
+    public BigDecimal getTotalIncomeForYear(int year) {
+        return getTransactionsForYear(year).stream()
+                .filter(tx ->  tx.getType() == TransactionType.INCOME)
+                .map(Transaction::getAmount)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+    }
+    public BigDecimal getTotalExpenseForYear(int year) {
+        return getTransactionsForYear(year).stream()
+                .filter(tx -> tx.getType() == TransactionType.EXPENSE)
+                .map(Transaction::getAmount)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+    }
+
+    public BigDecimal getRemainingForYear(int year) {
+        BigDecimal totalIncome = getTotalIncomeForYear(year);
+        BigDecimal totalExpense = getTotalExpenseForYear(year);
+        return totalIncome.subtract(totalExpense);
     }
 
 }
