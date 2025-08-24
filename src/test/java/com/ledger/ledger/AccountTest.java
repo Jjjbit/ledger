4e4983fe3e73e85418a374c7fed81e94b353b792
daiji <jialu.dai@edu.unifi.it>
2025-08-24 18:40:06 +0200

    add test delete CreditAccount with InstallmentPlan

--- src/test/java/com/ledger/ledger/AccountTest.java
+++ src/test/java/com/ledger/ledger/AccountTest.java
@@ -16,6 +16,7 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.Optional;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -162,11 +163,147 @@ public class AccountTest {
         Assertions.assertEquals(new BigDecimal("1001.30"), updateUser.getNetAssets());
     }
 
-    //TODO: test delete LoanAccount, CreditAccount with debt and installment plan
 
     @Test
     @WithMockUser(username = "Alice") // Simulating an authenticated user
-    public void testDeleteAccountWithTransactions() throws Exception {
+    public void testDeleteCreditAccountWithTransactions() throws Exception {
+        Account account = new CreditAccount("Test Account",
+                BigDecimal.valueOf(1000),
+                testUser,
+                null,
+                true,
+                true,
+                BigDecimal.valueOf(1000),
+                BigDecimal.valueOf(100), // current debt
+                null,
+                null,
+                AccountType.CREDIT_CARD
+        );
+        accountRepository.save(account);
+
+        InstallmentPlan installmentPlan = new InstallmentPlan(
+                BigDecimal.valueOf(1200), // total amount
+                12, // total periods
+                BigDecimal.valueOf(0), // fee rate
+                1, // repaid periods
+                InstallmentPlan.FeeStrategy.EVENLY_SPLIT,
+                account // linked account
+        );
+        installmentPlanRepository.save(installmentPlan);
+        ((CreditAccount) account).addInstallmentPlan(installmentPlan);
+
+        // Add transactions to the account
+        Transaction transaction1 = new Expense(LocalDate.now(), BigDecimal.valueOf(10),null, account, testLedger, foodCategory);
+        Transaction transaction2 = new Income(LocalDate.now(), BigDecimal.valueOf(1500), null, account, testLedger, salaryCategory);
+        transactionRepository.save(transaction1);
+        transactionRepository.save(transaction2);
+
+        account.addTransaction(transaction1);
+        account.addTransaction(transaction2);
+
+        accountRepository.save(account);
+
+        mockMvc.perform(delete("/accounts/" + account.getId())
+                        .param("deleteTransactions", "true")
+                        .principal(() -> "Alice"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Account and associated transactions deleted successfully"));
+
+        Account deletedAccount1 = accountRepository.findById(account.getId()).orElse(null);
+        Assertions.assertNull(deletedAccount1);
+
+        User updatedUser = userRepository.findById(testUser.getId()).orElse(null);
+        Assertions.assertEquals(0, updatedUser.getTotalAssets().intValue());
+        Assertions.assertEquals(0, updatedUser.getTotalLiabilities().intValue());
+        Assertions.assertEquals(0, updatedUser.getNetAssets().intValue());
+
+        InstallmentPlan plan = installmentPlanRepository.findById(installmentPlan.getId()).orElse(null);
+        Assertions.assertNull(plan);
+
+        Transaction deletedTransaction1 = transactionRepository.findById(transaction1.getId()).orElse(null);
+        Assertions.assertNull(deletedTransaction1);
+
+        Transaction deletedTransaction2 = transactionRepository.findById(transaction2.getId()).orElse(null);
+        Assertions.assertNull(deletedTransaction2);
+
+        Assertions.assertEquals(0, transactionRepository.findByLedgerId(testLedger.getId()).size());
+        Assertions.assertEquals(0, transactionRepository.findByCategoryId(foodCategory.getId()).size());
+        Assertions.assertEquals(0, transactionRepository.findByCategoryId(salaryCategory.getId()).size());
+    }
+
+    @Test
+    @WithMockUser(username = "Alice") // Simulating an authenticated user
+    public void testDeleteCreditAccountWithoutTransactions() throws Exception{
+        Account account = new CreditAccount("Test Account",
+                BigDecimal.valueOf(1000),
+                testUser,
+                null,
+                true,
+                true,
+                BigDecimal.valueOf(1000),
+                BigDecimal.valueOf(100), // current debt
+                null,
+                null,
+                AccountType.CREDIT_CARD
+        );
+        accountRepository.save(account);
+
+        InstallmentPlan installmentPlan = new InstallmentPlan(
+                BigDecimal.valueOf(1200), // total amount
+                12, // total periods
+                BigDecimal.valueOf(0), // fee rate
+                1, // repaid periods
+                InstallmentPlan.FeeStrategy.EVENLY_SPLIT,
+                account // linked account
+        );
+        installmentPlanRepository.save(installmentPlan);
+        ((CreditAccount) account).addInstallmentPlan(installmentPlan);
+
+        // Add transactions to the account
+        Transaction transaction1 = new Expense(LocalDate.now(), BigDecimal.valueOf(10),null, account, testLedger, foodCategory);
+        Transaction transaction2 = new Income(LocalDate.now(), BigDecimal.valueOf(1500), null, account, testLedger, salaryCategory);
+        transactionRepository.save(transaction1);
+        transactionRepository.save(transaction2);
+
+        account.addTransaction(transaction1);
+        account.addTransaction(transaction2);
+
+        accountRepository.save(account);
+
+        mockMvc.perform(delete("/accounts/" + account.getId())
+                        .param("deleteTransactions", "false")
+                        .principal(() -> "Alice"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Account disassociated from transactions and deleted successfully"));
+
+        Account deletedAccount = accountRepository.findById(account.getId()).orElse(null); //cerca account da database
+        Assertions.assertNull(deletedAccount);
+
+        InstallmentPlan plan = installmentPlanRepository.findById(installmentPlan.getId()).orElse(null);
+        Assertions.assertNull(plan);
+
+        User updatedUser = userRepository.findById(testUser.getId()).orElse(null);
+        Assertions.assertEquals(0, updatedUser.getTotalAssets().compareTo(new BigDecimal("1490")));
+        Assertions.assertEquals(0, updatedUser.getTotalLiabilities().compareTo(BigDecimal.ZERO));
+        Assertions.assertEquals(0, updatedUser.getNetAssets().compareTo(new BigDecimal("1490")));
+
+        Transaction updatedTransaction1 = transactionRepository.findById(transaction1.getId()).orElse(null);
+        Assertions.assertNotNull(updatedTransaction1);
+
+        Transaction updatedTransaction2 = transactionRepository.findById(transaction2.getId()).orElse(null);
+        Assertions.assertNotNull(updatedTransaction2);
+
+        Assertions.assertEquals(2, transactionRepository.findByLedgerId(testLedger.getId()).size());
+        Assertions.assertEquals(1, transactionRepository.findByCategoryId(foodCategory.getId()).size());
+        Assertions.assertEquals(1, transactionRepository.findByCategoryId(salaryCategory.getId()).size());
+    }
+
+
+    //TODO: test delete LoanAccount
+
+    @Test
+    @WithMockUser(username = "Alice") // Simulating an authenticated user
+    public void testDeleteBasicAccountWithTransactions() throws Exception {
         Account account = new BasicAccount("Test Account",
                 BigDecimal.valueOf(1000),
                 null,
@@ -214,7 +351,7 @@ public class AccountTest {
 
     @Test
     @WithMockUser(username = "Alice") // Simulating an authenticated user
-    public void testDeleteAccountWithoutTransactions() throws Exception {
+    public void testDeleteBasicAccountWithoutTransactions() throws Exception {
         Account account = new BasicAccount("Test Account",
                 BigDecimal.valueOf(1000),
                 null,
@@ -247,7 +384,7 @@ public class AccountTest {
         Assertions.assertNull(deletedAccount);
 
         User updatedUser = userRepository.findById(testUser.getId()).orElse(null);
-        Assertions.assertEquals(new BigDecimal("1490"), updatedUser.getTotalAssets());
+        Assertions.assertEquals(0, updatedUser.getTotalAssets().compareTo(new BigDecimal("1490")));
 
         Transaction updatedTransaction1 = transactionRepository.findById(transaction1.getId()).orElse(null);
         Assertions.assertNotNull(updatedTransaction1);
@@ -667,7 +804,7 @@ public class AccountTest {
                 BigDecimal.valueOf(0), // fee rate
                 1, // repaid periods
                 InstallmentPlan.FeeStrategy.EVENLY_SPLIT,
-                creditAccount
+                creditAccount // linked account
         );
         installmentPlanRepository.save(installmentPlan);
 
@@ -677,24 +814,93 @@ public class AccountTest {
 
         mockMvc.perform(put("/accounts/" + creditAccount.getId() + "/repay-installment-plan")
                         .param("installmentPlanId", installmentPlan.getId().toString())
-                        .principal(() -> "Alice"))
+                        .principal(() -> "Alice")
+                        .param("amount", ""))
                 .andExpect(status().isOk())
                 .andExpect(content().string("Installment plan repaid successfully"));
 
         Account updateCreditAccount=accountRepository.findByName("account2");
-        Assertions.assertNotNull(updateCreditAccount);
         Assertions.assertEquals(new BigDecimal("1000.00"), ((CreditAccount)updateCreditAccount).getCurrentDebt());
         Assertions.assertEquals(2, ((CreditAccount)updateCreditAccount).getInstallmentPlans().get(0).getPaidPeriods());
 
+        InstallmentPlan updateInstallmentPlan=installmentPlanRepository.findById(installmentPlan.getId()).orElse(null);
+        Assertions.assertEquals(2, updateInstallmentPlan.getPaidPeriods());
 
         User updateUser=userRepository.findById(testUser.getId()).orElse(null);
         Assertions.assertEquals(new BigDecimal("900.00"), updateUser.getTotalAssets());
         Assertions.assertEquals(new BigDecimal("1000.00"), updateUser.getTotalLiabilities());
         Assertions.assertEquals(new BigDecimal("-100.00"), updateUser.getNetAssets());
+
+        //repayInstallmentPlan with specific amount
+        mockMvc.perform(put("/accounts/" + creditAccount.getId() + "/repay-installment-plan")
+                        .param("installmentPlanId", installmentPlan.getId().toString())
+                        .principal(() -> "Alice")
+                        .param("amount", "200"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Installment plan repaid successfully"));
+
+        Account updateCreditAccount2=accountRepository.findByName("account2");
+        Assertions.assertEquals(new BigDecimal("800.00"), ((CreditAccount)updateCreditAccount2).getCurrentDebt());
+        Assertions.assertEquals(4, ((CreditAccount)updateCreditAccount2).getInstallmentPlans().get(0).getPaidPeriods());
+
+        InstallmentPlan updateInstallmentPlan2=installmentPlanRepository.findById(installmentPlan.getId()).orElse(null);
+        Assertions.assertEquals(4, updateInstallmentPlan2.getPaidPeriods());
+
+        User updateUser2=userRepository.findById(testUser.getId()).orElse(null);
+        Assertions.assertEquals(new BigDecimal("700.00"), updateUser2.getTotalAssets());
+        Assertions.assertEquals(new BigDecimal("800.00"), updateUser2.getTotalLiabilities());
+        Assertions.assertEquals(new BigDecimal("-100.00"), updateUser2.getNetAssets());
     }
 
-    //TODO:  repayLoan for LoanAccount
     @Test
     @WithMockUser(username = "Alice")
-    public void testRepayLoan() throws Exception{}
+    public void testRepayLoan() throws Exception{
+        //test repayLoan LoanAccount
+        Account loanAccount = new LoanAccount("account2",
+                testUser,
+                null,
+                true,
+                36,
+                1,
+                BigDecimal.valueOf(1), //annual interest rate
+                BigDecimal.valueOf(100), //loan amount
+                null, //receiving account
+                LocalDate.now(),
+                LoanAccount.RepaymentType.EQUAL_INTEREST
+        );
+        accountRepository.save(loanAccount);
+
+        mockMvc.perform(put("/accounts/" + loanAccount.getId() + "/repay-loan")
+                        .principal(() -> "Alice")
+                        .param("fromAccountId", "")
+                        .param("amount", ""))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Loan repaid successfully"));
+
+        Account updateLoanAccount=accountRepository.findByName("account2");
+        Assertions.assertEquals(2, ((LoanAccount)updateLoanAccount).getRepaidPeriods());
+        Assertions.assertEquals(new BigDecimal("95.88"), ((LoanAccount)updateLoanAccount).getRemainingLoanAmount());
+
+        User updateUser=userRepository.findById(testUser.getId()).orElse(null);
+        Assertions.assertEquals(new BigDecimal("0"), updateUser.getTotalAssets());
+        Assertions.assertEquals(new BigDecimal("95.88"), updateUser.getTotalLiabilities());
+        Assertions.assertEquals(new BigDecimal("-95.88"), updateUser.getNetAssets());
+
+        mockMvc.perform(put("/accounts/" + loanAccount.getId() + "/repay-loan")
+                        .principal(() -> "Alice")
+                        .param("fromAccountId", "")
+                        .param("amount", "28.5"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Loan repaid successfully"));
+
+        Account updateLoanAccount2=accountRepository.findByName("account2");
+        Assertions.assertEquals(12, ((LoanAccount)updateLoanAccount2).getRepaidPeriods());
+        Assertions.assertEquals(new BigDecimal("67.68"), ((LoanAccount)updateLoanAccount2).getRemainingLoanAmount());
+
+        User updateUser2=userRepository.findById(testUser.getId()).orElse(null);
+        Assertions.assertEquals(new BigDecimal("0"), updateUser2.getTotalAssets());
+        Assertions.assertEquals(new BigDecimal("67.68"), updateUser2.getTotalLiabilities());
+        Assertions.assertEquals(new BigDecimal("-67.68"), updateUser2.getNetAssets());
+
+    }
 }
