0360e257a40e709b20efca08bb69941a46dc414e
daiji <jialu.dai@edu.unifi.it>
2025-08-27 00:48:55 +0200

    delete BorrowingAndLendingRepository

--- src/main/java/com/ledger/ledger/repository/BorrowingAndLendingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ledger.ledger.repository;
-
-import com.ledger.ledger.domain.BorrowingAndLending;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BorrowingAndLendingRepository extends JpaRepository<BorrowingAndLending, Long> {
-}
