342ed7ce75bc87494947ba9acc99f669f222a23d
daiji <jialu.dai@edu.unifi.it>
2025-09-19 18:05:57 +0200

    complete InstallmentPlanController.java

--- src/main/java/com/ledger/ledger/business/InstallmentPlanController.java
+++ src/main/java/com/ledger/ledger/business/InstallmentPlanController.java
@@ -1,5 +1,272 @@
 package com.ledger.ledger.business;
 
+import com.ledger.ledger.domain.*;
+import com.ledger.ledger.repository.AccountRepository;
+import com.ledger.ledger.repository.InstallmentPlanRepository;
+import com.ledger.ledger.repository.LedgerRepository;
+import com.ledger.ledger.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.security.Principal;
+import java.time.LocalDate;
+
+@RestController
+@RequestMapping("/installment-plans")
 public class InstallmentPlanController {
+    @Autowired
+    private InstallmentPlanRepository installmentPlanRepository;
+    @Autowired
+    private AccountRepository accountRepository;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private LedgerRepository ledgerRepository;
+
+    @PostMapping("/create")
+    @Transactional
+    @PreAuthorize("isAuthenticated()")
+    public ResponseEntity<String> createInstallmentPlan(Principal principal,
+                                                        @RequestParam BigDecimal totalAmount,
+                                                        @RequestParam int totalPeriods,
+                                                        @RequestParam (required = false, defaultValue = "0") Integer paidPeriods,
+                                                        @RequestParam(required = false, defaultValue = "0.00") BigDecimal feeRate,
+                                                        @RequestParam(required = false, defaultValue = "EVENLY_SPLIT") InstallmentPlan.FeeStrategy feeStrategy,
+                                                        @RequestParam Long linkedAccountId) {
+        if(principal == null){
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Unauthorized access");
+        }
+
+        User user=userRepository.findByUsername(principal.getName());
+        if (user == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Unauthorized access");
+        }
+
+        if(linkedAccountId == null){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Linked account is required");
+        }
+        Account linkedAccount = accountRepository.findById(linkedAccountId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Linked account not found"));
+        if (!linkedAccount.getOwner().getId().equals(user.getId())) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("account does not belong to user");
+        }
+        if(!(linkedAccount instanceof CreditAccount)){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Linked account must be a credit account");
+        }
+        if(totalAmount.compareTo(BigDecimal.ZERO)<=0 || totalPeriods<=0 || paidPeriods<0 || paidPeriods>totalPeriods || feeRate.compareTo(BigDecimal.ZERO)<0){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid parameters");
+        }
+
+        InstallmentPlan installmentPlan = new InstallmentPlan(totalAmount,
+                totalPeriods,
+                feeRate,
+                paidPeriods,
+                feeStrategy,
+                linkedAccount);
+        installmentPlanRepository.save(installmentPlan);
+        ((CreditAccount) linkedAccount).addInstallmentPlan(installmentPlan);
+        //((CreditAccount) linkedAccount).getInstallmentPlans().add(installmentPlan);
+        //((CreditAccount) linkedAccount).setCurrentDebt(((CreditAccount) linkedAccount).getCurrentDebt().add(installmentPlan.getRemainingAmount()).setScale(2, RoundingMode.HALF_UP));
+        accountRepository.save(linkedAccount);
+
+        return ResponseEntity.ok("installment plan created successfully");
+
+    }
+
+    @PutMapping("/{id}/edit")
+    @Transactional
+    @PreAuthorize("isAuthenticated()")
+    public ResponseEntity<String> editInstallmentPlan(Principal principal,
+                                                      @PathVariable Long id,
+                                                      @RequestParam(required = false) BigDecimal totalAmount,
+                                                      @RequestParam(required = false) Integer totalPeriods,
+                                                      @RequestParam(required = false) Integer paidPeriods,
+                                                      @RequestParam(required = false) BigDecimal feeRate,
+                                                      @RequestParam(required = false) InstallmentPlan.FeeStrategy feeStrategy,
+                                                      @RequestParam(required = false) Long linkedAccountId){
+        if(principal == null){
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Unauthorized access");
+        }
+        User user=userRepository.findByUsername(principal.getName());
+        if (user == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Unauthorized access");
+        }
+
+        InstallmentPlan installmentPlan = installmentPlanRepository.findById(id).orElse(null);
+        if (installmentPlan == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Installment plan not found");
+        }
+        if(! installmentPlan.getLinkedAccount().getOwner().getId().equals(user.getId())){
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("You do not have permission to edit this installment plan");
+        }
+        BigDecimal oldRemainingAmount = installmentPlan.getRemainingAmount();
+
+        if (totalAmount != null){
+            installmentPlan.setTotalAmount(totalAmount);
+        }
+        if (totalPeriods != null){
+            installmentPlan.setTotalPeriods(totalPeriods);
+        }
+        if (paidPeriods != null){
+            installmentPlan.setPaidPeriods(paidPeriods);
+        }
+        if (feeRate != null){
+            installmentPlan.setFeeRate(feeRate);
+        }
+        if (feeStrategy != null){
+            installmentPlan.setFeeStrategy(feeStrategy);
+        }
+        installmentPlan.setRemainingAmount(installmentPlan.getRemainingAmountWithRepaidPeriods());
+        if (linkedAccountId != null){// change linked account
+            Account creditAccount = accountRepository.findById(linkedAccountId)
+                    .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Linked account not found"));
+            if ( !creditAccount.getOwner().getId().equals(user.getId())) {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("account does not belong to user");
+            }
+            if(!(creditAccount instanceof CreditAccount)){
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Linked account must be a credit account");
+            }
+            if(installmentPlan.getLinkedAccount() != null){
+                CreditAccount oldAccount = (CreditAccount) installmentPlan.getLinkedAccount();
+                oldAccount.setCurrentDebt(oldAccount.getCurrentDebt().subtract(oldRemainingAmount).setScale(2, RoundingMode.HALF_UP));
+                oldAccount.getInstallmentPlans().remove(installmentPlan);
+                accountRepository.save(oldAccount);
+            }
+
+            ((CreditAccount) creditAccount).addInstallmentPlan(installmentPlan);
+            //((CreditAccount) creditAccount).setCurrentDebt(((CreditAccount) creditAccount).getCurrentDebt().add(installmentPlan.getRemainingAmount()).setScale(2, RoundingMode.HALF_UP));
+            //((CreditAccount) creditAccount).getInstallmentPlans().add(installmentPlan);
+            accountRepository.save(creditAccount);
+            installmentPlan.setLinkedAccount(creditAccount);
+        }else{ // no change to linked account
+            // if other parameters change, need to update the linked account's current debt
+            CreditAccount creditAccount = (CreditAccount) installmentPlan.getLinkedAccount();
+            creditAccount.setCurrentDebt(creditAccount.getCurrentDebt().subtract(oldRemainingAmount).add(installmentPlan.getRemainingAmount()).setScale(2, RoundingMode.HALF_UP));
+            accountRepository.save(creditAccount);
+        }
+        installmentPlanRepository.save(installmentPlan);
+
+        return ResponseEntity.ok("installment plan updated successfully");
+
+    }
+
+    @DeleteMapping("/{id}/delete")
+    @Transactional
+    @PreAuthorize("isAuthenticated()")
+    public ResponseEntity<String> deleteInstallmentPlan(Principal principal,
+                                                        @PathVariable Long id){
+        if(principal == null){
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Unauthorized access");
+        }
+        User user=userRepository.findByUsername(principal.getName());
+        if (user == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Unauthorized access");
+        }
+        InstallmentPlan installmentPlan = installmentPlanRepository.findById(id).orElse(null);
+        if (installmentPlan == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Installment plan not found");
+        }
+
+        if(! installmentPlan.getLinkedAccount().getOwner().getId().equals(user.getId())){
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("You do not have permission to delete this installment plan");
+        }
+
+        CreditAccount creditAccount = (CreditAccount) installmentPlan.getLinkedAccount();
+        creditAccount.removeInstallmentPlan(installmentPlan);
+        installmentPlan.setLinkedAccount(null);
+        //creditAccount.setCurrentDebt(creditAccount.getCurrentDebt().subtract(installmentPlan.getRemainingAmount()).setScale(2, RoundingMode.HALF_UP));
+        //creditAccount.getInstallmentPlans().remove(installmentPlan);
+        accountRepository.save(creditAccount);
+
+        installmentPlanRepository.delete(installmentPlan);
+
+        return ResponseEntity.ok("installment plan deleted successfully");
+    }
+
+    @PutMapping("{id}/repay")
+    @Transactional
+    @PreAuthorize("isAuthenticated()")
+    public ResponseEntity<String> repayInstallmentPlan(@PathVariable Long id,
+                                                       @RequestParam Long creditAccountId,
+                                                       Principal principal,
+                                                       @RequestParam (required = false) BigDecimal amount,
+                                                       @RequestParam (required = false) Long ledgerId) {
+        if(principal == null){
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Unauthorized access");
+        }
+        User user = userRepository.findByUsername(principal.getName());
+        if (user == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Unauthorized access");
+        }
+
+        CreditAccount account = (CreditAccount) accountRepository.findById(creditAccountId).orElse(null);
+        if (account == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Credit account not found");
+        }
+        InstallmentPlan installmentPlan = installmentPlanRepository.findById(id).orElse(null);
+        if (installmentPlan == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Installment plan not found");
+        }
+
+        if (!account.getOwner().equals(user)) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("You cannot repay someone else's installment plan");
+        }
+
+        if (!account.getInstallmentPlans().contains(installmentPlan)) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Installment plan does not belong to this account");
+        }
+
+
+        Ledger ledger=null;
+        if(ledgerId != null) {
+            ledger= ledgerRepository.findById(ledgerId).orElse(null);
+        }
+        if(installmentPlan.getPaidPeriods() >= installmentPlan.getTotalPeriods()){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Installment plan already fully paid");
+        }
+        if(installmentPlan.getRemainingAmount().compareTo(BigDecimal.ZERO)<=0){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Installment plan already fully paid");
+        }
+
+        if(amount == null){ //pay one period
+            amount=installmentPlan.getMonthlyPayment(installmentPlan.getPaidPeriods() + 1);
+            installmentPlan.repayOnePeriod(); // incrementa paid periods di 1 e decrementa remaining amount
+        }else{
+            if(amount.compareTo(BigDecimal.ZERO)<=0){
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Amount must be positive");
+            }
+            if(amount.compareTo(installmentPlan.getRemainingAmount())>0){
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Amount exceeds remaining amount of the installment plan");
+            }
+
+            installmentPlan.repayPartial(amount);// decrementa remaining amount e incrementa paid periods
+        }
+        Transaction tx = new Transfer(
+                LocalDate.now(),
+                "Repay installment plan",
+                account,
+                null,
+                amount,
+                ledger
+        );
+        account.debit(amount);
+        account.getOutgoingTransactions().add(tx);
+        account.setCurrentDebt(account.getCurrentDebt().subtract(amount).setScale(2, RoundingMode.HALF_UP));
+        if(ledger != null){
+            ledger.getTransactions().add(tx);
+            ledgerRepository.save(ledger);
+        }
+        installmentPlanRepository.save(installmentPlan);
+        accountRepository.save(account);
+        return ResponseEntity.ok("Installment plan repaid successfully");
+    }
+
 
 }
